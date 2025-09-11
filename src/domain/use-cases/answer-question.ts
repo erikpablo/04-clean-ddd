2b7c075886e72dbfbfdbfeed3df8d0f08636f2d1
Erik Pablo <nunespablo470@gmail.com>
2025-09-11 09:08:33 -0300

    feat: :sparkles: external dependencies

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,4 +1,5 @@
 import { Answer } from '../entities/answer'
+import type { AnswersRepository } from '../repositories/answers-repository'
 
 interface AnswerQuestionUseCaseRequest {
   questionId: string
@@ -7,12 +8,15 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
+  constructor(private answersRepository: AnswersRepository) {}
   execute({ questionId, instructorId, content }: AnswerQuestionUseCaseRequest) {
     const answer = new Answer({
       content,
       authorId: instructorId,
       questionId,
     })
+
+    this.answersRepository.create(answer)
 
     return answer
   }
