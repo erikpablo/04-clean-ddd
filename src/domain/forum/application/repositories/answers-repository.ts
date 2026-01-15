53df9a6af48b89bdd8dd62b5ae42c6730aca0981
Erik Pablo Nunes <nunespablo470@gmail.com>
2026-01-15 17:57:39 -0300

    feat: :sparkles: add nem use case for fetch recent questions and fetch questions answers

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,8 +1,13 @@
-import type { Answer } from '../../enterprise/entities/answer'
+import type { PaginationParams } from '@/core/repositories/pagination-params'
+import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
   findById(id: string): Promise<Answer | null>
   create(answer: Answer): Promise<void>
   save(answer: Answer): Promise<void>
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams
+  ): Promise<Answer[]>
   delete(answer: Answer): Promise<void>
 }
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,7 +1,8 @@
-import type { QuestionRepository } from '@/domain/forum/application/repositories/questions-repository'
-import type { Question } from '@/domain/forum/enterprise/entities/question'
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
 
-export class InMemoryQuestionsRepository implements QuestionRepository {
+export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
   async save(question: Question) {
@@ -18,6 +19,14 @@ export class InMemoryQuestionsRepository implements QuestionRepository {
     }
 
     return question
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20)
+
+    return questions
   }
 
   async findBySlug(slug: string) {
