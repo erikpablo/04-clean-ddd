136824c0c15314ca323586521412b72a00115682
Erik Pablo Nunes <nunespablo470@gmail.com>
2026-02-10 13:27:58 -0300

    feat: :sparkles: add new repository

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,5 +1,7 @@
 import { QuestionComment } from '../../enterprise/entities/question-comments'
 
 export interface QuestionCommentRepository {
+  findById(id: string): Promise<QuestionComment | null>
   create(questionComment: QuestionComment): Promise<void>
+  delete(questionComment: QuestionComment): Promise<void>
 }
